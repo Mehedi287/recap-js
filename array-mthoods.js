15611ac55d6604ae2a1befda0f26d699d8fe55d8
Mehedi390 <mh0816150@gmail.com>
2021-09-11 01:56:55 -0700

    fast commit

---
+++ array-mthoods.js
@@ -0,0 +1,12 @@
+const products = [
+    { naem: 'laptop', id: 085679, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 67889, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 6489, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 64589, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 89890, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 897089, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 789089, price: 98, brand: 'asus' },
+    { naem: 'laptop', id: 8789, price: 98, brand: 'asus' },
+]
+const id = products.map(product => product.id);
+console.log(id);
